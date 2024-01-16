0b3e0c02dd941d2c60d18c655a7d24b97f509e00
Glydric <git@miglio.dev>
2024-01-16 17:22:00 +0100

    created a getEntireDataset that merges the data for us and minor improvements

---
+++ scripts/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+
+    # print("prod\n")
+    # print(productions.head())
+
+    # fermate = getFermate("618", "22", "11")
+    # print("fermate\n")
+    # print(fermate.head())
+
+    # energy = getEnergy("618", "22", "11", "04")
+    # print("energy\n")
+    # print(energy.head())
